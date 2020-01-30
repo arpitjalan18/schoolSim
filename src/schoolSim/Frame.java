3707dba8d45870a698c5e1060daf071ef0be7cd4
arpit jalan <arpitoburrito@gmail.com>
2020-01-29 21:22:00 -0800

    Rough final no buttons portals fixed

--- src/schoolSim/Frame.java
+++ src/schoolSim/Frame.java
@@ -28,6 +28,7 @@ public class Frame implements ActionListener{
 
 	public dotArray dots = new dotArray();
 	public ArrayList<classes> cl = new ArrayList<>();
+	public ArrayList<classes> cl2 = new ArrayList<>();
 	public int c, nc, x, nx, y, ny;
 	
 	public void initializeClass()
@@ -165,43 +166,210 @@ public class Frame implements ActionListener{
 		//second floor
 		
 	}
+	public void initializeClass2()
+	{
+		//yellow buildings
+		cl2.add(new classes(21, 44, 1));
+		cl2.add(new classes(25, 44, 1));
+		cl2.add(new classes(29, 44, 1));
+		cl2.add(new classes(21, 53, 1));
+		cl2.add(new classes(25, 53, 1));
+		cl2.add(new classes(29, 53, 1));
+		//blue buildings
+		cl2.add(new classes(30, 64 , 1));
+		cl2.add(new classes(30, 70, 1));
+		cl2.add(new classes(34, 64, 1));
+		cl2.add(new classes(34, 70, 1));
+		cl2.add(new classes(38, 64, 1));
+		cl2.add(new classes(38, 70, 1));
+		cl2.add(new classes(45, 76, 1));
+		cl2.add(new classes(49, 76, 1));
+		cl2.add(new classes(45, 80, 1));
+		cl2.add(new classes(49, 80, 1));
+		cl2.add(new classes(45, 84, 1));
+		cl2.add(new classes(49, 84, 1));
+		cl2.add(new classes(45, 88, 1));
+		cl2.add(new classes(49, 88, 1));
+		cl2.add(new classes(45, 92, 1));
+		cl2.add(new classes(49, 92, 1));
+		//gray buildings
+		cl2.add(new classes(61, 23, 1));
+		cl2.add(new classes(61, 27, 1));
+		cl2.add(new classes(67, 16, 1));
+		cl2.add(new classes(71, 16, 1));
+		cl2.add(new classes(75, 16, 1));
+		cl2.add(new classes(79, 16, 1));
+		cl2.add(new classes(83, 16, 1));
+		cl2.add(new classes(87, 16, 1));
+		cl2.add(new classes(91, 16, 1));
+		cl2.add(new classes(95, 16, 1));
+		cl2.add(new classes(99, 16, 1));
+		cl2.add(new classes(103, 16, 1));
+		cl2.add(new classes(67, 23, 1));
+		cl2.add(new classes(71, 23, 1));
+		cl2.add(new classes(75, 23, 1));
+		cl2.add(new classes(79, 23, 1));
+		cl2.add(new classes(91, 23, 1));
+		cl2.add(new classes(95, 23, 1));
+		cl2.add(new classes(99, 23, 1));
+		cl2.add(new classes(103, 23, 1));
+		cl2.add(new classes(61, 59, 1));
+		cl2.add(new classes(61, 63, 1));
+		cl2.add(new classes(67, 63, 1));
+		cl2.add(new classes(71, 63, 1));
+		cl2.add(new classes(75, 63, 1));
+		cl2.add(new classes(79, 63, 1));
+		cl2.add(new classes(91, 63, 1));
+		cl2.add(new classes(95, 63, 1));
+		cl2.add(new classes(99, 63, 1));
+		cl2.add(new classes(103, 63, 1));
+		cl2.add(new classes(109, 59, 1));
+		cl2.add(new classes(109, 63, 1));
+		cl2.add(new classes(67, 70, 1));
+		cl2.add(new classes(71, 70, 1));
+		cl2.add(new classes(75, 70, 1));
+		cl2.add(new classes(79, 70, 1));
+		cl2.add(new classes(83, 70, 1));
+		cl2.add(new classes(87, 70, 1));
+		cl2.add(new classes(91, 70, 1));
+		cl2.add(new classes(95, 70, 1));
+		cl2.add(new classes(99, 70, 1));
+		cl2.add(new classes(103, 70, 1));
+		cl2.add(new classes(109, 23, 1));
+		cl2.add(new classes(109, 27, 1));
+		//66 classes		
+	
+		//blue 2
+		cl2.add(new classes(30, 64 , 2));
+		cl2.add(new classes(30, 70, 2));
+		cl2.add(new classes(34, 64, 2));
+		cl2.add(new classes(34, 70, 2));
+		cl2.add(new classes(38, 64, 2));
+		cl2.add(new classes(38, 70, 2));
+		cl2.add(new classes(45, 76, 2));
+		cl2.add(new classes(49, 76, 2));
+		cl2.add(new classes(45, 80, 2));
+		cl2.add(new classes(49, 80, 2));
+		cl2.add(new classes(45, 84, 2));
+		cl2.add(new classes(49, 84, 2));
+		cl2.add(new classes(45, 88, 2));
+		cl2.add(new classes(49, 88, 2));
+		cl2.add(new classes(45, 92, 2));
+		cl2.add(new classes(49, 92, 2));
+		
+
+		//grey 2
+		cl2.add(new classes(67, 16, 2));
+		cl2.add(new classes(71, 16, 2));
+		cl2.add(new classes(75, 16, 2));
+		cl2.add(new classes(79, 16, 2));
+		cl2.add(new classes(83, 16, 2));
+		cl2.add(new classes(87, 16, 2));
+		cl2.add(new classes(91, 16, 2));
+		cl2.add(new classes(95, 16, 2));
+		cl2.add(new classes(99, 16, 2));
+		cl2.add(new classes(103, 16, 2));
+		cl2.add(new classes(67, 23, 2));
+		cl2.add(new classes(71, 23, 2));
+		cl2.add(new classes(75, 23, 2));
+		cl2.add(new classes(79, 23, 2));
+		cl2.add(new classes(91, 23, 2));
+		cl2.add(new classes(95, 23, 2));
+		cl2.add(new classes(99, 23, 2));
+		cl2.add(new classes(103, 23, 2));
+
+		cl2.add(new classes(67, 63, 2));
+		cl2.add(new classes(71, 63, 2));
+		cl2.add(new classes(75, 63, 2));
+		cl2.add(new classes(79, 63, 2));
+		cl2.add(new classes(91, 63, 2));
+		cl2.add(new classes(95, 63, 2));
+		cl2.add(new classes(99, 63, 2));
+		cl2.add(new classes(103, 63, 2));
+
+		cl2.add(new classes(67, 70, 2));
+		cl2.add(new classes(71, 70, 2));
+		cl2.add(new classes(75, 70, 2));
+		cl2.add(new classes(79, 70, 2));
+		cl2.add(new classes(83, 70, 2));
+		cl2.add(new classes(87, 70, 2));
+		cl2.add(new classes(91, 70, 2));
+		cl2.add(new classes(95, 70, 2));
+		cl2.add(new classes(99, 70, 2));
+		cl2.add(new classes(103, 70, 2));
+
+		//second floor
+		
+	}
 
 
 	public void initializeDots() {
 		initializeClass();
+		int a = 2;
+		for (int i = 0; i < a; i++) {
+			initializeClass2();
+		}
 		int xAdd = -1, yAdd=-1;
-		for(int i = 0;  i < 594 ;i++)
-		{
-					
-			
-			c = (int)(Math.random()*(66));
-			nc = (int)(Math.random()*(66));
-			if(c==nc)
-			{
+		for (int i = 0; i < cl.size()*a; i++) {
+			System.out.println(cl.size() + "     " + cl2.size());
+			nc = (int)(Math.random()*(cl2.size())); 
+			while (cl.get(i/a).x == cl2.get(nc).x && cl.get(i/a).y == cl2.get(nc).y) {
+				nc = (int)(Math.random()*(cl2.size())); 
+				if (cl2.size() == 1)
+					break;
 			}
-			else
-			{
-			 x = cl.get(c).getX() +xAdd;
-			 y = cl.get(c).getY() + yAdd;
-			 nx = cl.get(nc).getX()+xAdd;
-			 ny = cl.get(nc).getY()+yAdd;
-			 
-			 xAdd++;
+		
+				nx = cl2.get(nc).getX()+xAdd;
+				ny = cl2.get(nc).getY()+yAdd;
+				int nf = cl2.get(nc).getfloorNum();
+				dots.add(new dot(Color.BLACK, cl.get(i/a).x+xAdd, cl.get(i/a).y+yAdd, nx, ny, cl.get(i/a).getfloorNum(), nf));
+				xAdd++;
 				if (xAdd==2) {
 					xAdd=-1; yAdd++;
 					if (yAdd ==2) {
 						yAdd=-1;
 					}
 				}
-			 
-			// dots.add(new dot(Color.BLACK, x, y, nx, ny, 1);
-			}
-		}	
-		for (int i = 0; i < cl.size();i++) {
-			if (/*cl.get(i).getfloorNum()*/true)
-				dots.add(new dot (Color.BLACK, cl.get(i).getX(), cl.get(i).getY(),  50, 50, cl.get(i).getfloorNum(), 1));
-		}	
+				
+			
+			cl2.remove(nc);
+				
+		}
+			
 	}
+	public void changeDotsDests() {
+		int a = 2;
+		for (int i = 0; i < a; i++) {
+			initializeClass2();
+		}
+		int xAdd = -1, yAdd=-1;
+		for (int i = 0; i < cl.size()*a; i++) {
+			System.out.println(cl.size() + "     " + cl2.size());
+			nc = (int)(Math.random()*(cl2.size())); 
+			while (cl.get(i/a).x == cl2.get(nc).x && cl.get(i/a).y == cl2.get(nc).y) {
+				nc = (int)(Math.random()*(cl2.size())); 
+				if (cl2.size() == 1)
+					break;
+			}
+		
+				nx = cl2.get(nc).getX()+xAdd;
+				ny = cl2.get(nc).getY()+yAdd;
+				int nf = cl2.get(nc).getfloorNum();
+				dots.dots.get(i).newDest(nx, ny,  nf);
+				xAdd++;
+				if (xAdd==2) {
+					xAdd=-1; yAdd++;
+					if (yAdd ==2) {
+						yAdd=-1;
+					}
+				}
+				
+			
+			cl2.remove(nc);
+				
+		}
+	}
+	
 	public Frame(){
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int screenHeight = screenSize.height;
@@ -212,7 +380,7 @@ public class Frame implements ActionListener{
 		dots.setHeight(screenHeight);
 		frame.add(dots);
 		initializeDots();
-		colorByGender();
+		colorByGrade();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		timer.start();		  
 		
@@ -249,18 +417,28 @@ public class Frame implements ActionListener{
 		for (int i = 0; i < dots.dots.size(); i++) {
 			dotPositions.add(new Point(dots.dots.get(i).xStart, dots.dots.get(i).yStart));
 		}
+		boolean stillMoving = false;
 		for (int i = 0; i < dots.dots.size(); i++) {
 				int otherDots = -1;	
 			
 				for (Point x : dotPositions){
 					if(x.x == dotPositions.get(i).x && x.y ==dotPositions.get(i).y) {otherDots++;}
 				}
-				if (ticks%(10 + otherDots*2) == 0) {
+				if (ticks%(1 + otherDots*1) == 0) {
+					if(dots.dots.get(i).path.size()> 0) {
+						stillMoving = true;
+					}
+				
 					dots.dots.get(i).updatePosition();
 				}
-
-				dots.dots.get(0).repaint();
-				
+				else {
+					stillMoving = true;
+				}
+				dots.dots.get(0).repaint();					
+		}
+		if (stillMoving == false) {
+			System.out.println("done moving");
+			changeDotsDests();
 		}
 		dots.repaint();
 
--- src/schoolSim/dot.java
+++ src/schoolSim/dot.java
@@ -17,8 +17,8 @@ public class dot extends JPanel {
 	int yDest;
 	int floorDest;
 	int floorStart;
-	ArrayList<Point> path;
-	Point x; 
+	ArrayList<Point> path = new ArrayList<>();
+	AStar runner;
 	int speedDiv;
 	public dot( Color color, int xStart, int yStart, int xDest, int yDest, int floorStart, int floorDest) {
 		
@@ -30,41 +30,219 @@ public class dot extends JPanel {
 		this.floorDest = floorDest;
 		this.floorStart = floorStart;
 		
-		AStar runner = new AStar();
-		path = runner.test(xStart, yStart, xDest, yDest, floorStart);
+		runner = new AStar();
 		
 		//needs to go down and back up
-		if (path.size() == 0 &&( floorDest == 2 && floorStart ==2) ) {
-			//do something
+		if (( floorDest == 2 && floorStart ==2) ) {
+			if ( runner.test(xStart, yStart, xDest, yDest, 2).size()==0) {
+				findPortalGoingDown();
+				findPortalGoingUp();
+				normalPathFind(2);
+			}
+			else {
+				normalPathFind(2);
+			}
 		}// 2nd to first but the coordinates technicaly still work
 		else if(floorDest > floorStart) {
-			//do something
-			//first to second
+			findPortalGoingUp();
+			normalPathFind(2);
 		}
 		else if(floorDest < floorStart) {
-			//do something
-			//second to first
+			
+			findPortalGoingDown();
+			normalPathFind(1);
+		
 		}
 		else {
-			
+			normalPathFind(1);
 		}
 		
-	}
-	
-	public void findPortal() {
 		
 	}
+	public void newDest(int x, int y, int floor) {
+		xDest = x;
+		yDest = y;
+		int floorDest = floor;
+		runner = new AStar();
+		
+		//needs to go down and back up
+		if (( floorDest == 2 && floorStart ==2) ) {
+			if ( runner.test(xStart, yStart, xDest, yDest, 2).size()==0) {
+				findPortalGoingDown();
+				findPortalGoingUp();
+				normalPathFind(2);
+			}
+			else {
+				normalPathFind(2);
+			}
+		}// 2nd to first but the coordinates technicaly still work
+		else if(floorDest > floorStart) {
+			findPortalGoingUp();
+			normalPathFind(2);
+		}
+		else if(floorDest < floorStart) {
+			
+			findPortalGoingDown();
+			normalPathFind(1);
+		
+		}
+		else {
+			normalPathFind(1);
+		}
+	}
+	public void addToPath(ArrayList<Point> addThis) {
+			for (Point point: addThis) {
+				path.add(point);
+			}
+	}
+	public void normalPathFind(int newFloor) {
+
+		if (path.size()> 1) {
+			
+			int x = path.get(1).x;
+			int y= path.get(1).y;
+			path.addAll(0, runner.test(x, y, xDest, yDest, newFloor));
+		}
+		else{path.addAll(0, runner.test(xStart, yStart, xDest, yDest, floorStart));}
+	}
+	public void findPortalGoingDown() {
+		int x ; int y;
+		if (path.size() > 0) {
+			 x = path.get(1).x;
+			 y = path.get(1).y;
+		}
+		else {
+			x = xStart;
+			y = yStart;
+		}
+		
+		if (xStart < 43) {
+			path.addAll(0, runner.test(x, y, 26, 67, 2));
+			
+		}
+		else if (xStart <  56) {
+			if (yStart < 85)
+				path.addAll(0, runner.test(x, y, 46, 67, 2));
+			if (yStart > 84)	{
+				path.addAll(0, runner.test(x, y, 52, 96, 2));
+			}
+		}
+		else if (yStart > 14 && yStart < 20) {
+			if (xStart < 86)  {
+				path.addAll(0, runner.test(x, y, 63, 16, 2));
+			}
+			if (xStart > 85) {
+				path.addAll(0, runner.test(x, y, 107, 16, 2));		
+			}
+		  }
+		else if (yStart > 19 && yStart < 37) {
+			if (xStart < 86)  {
+				path.addAll(0, runner.test(x, y, 67, 29, 2));
+				
+			}
+			if (xStart > 85) {
+				path.addAll(0, runner.test(x, y, 103, 29, 2));			
+			}
+		}
+		else if(yStart > 60 && yStart < 68) {
+			if (xStart < 86)  {
+				path.addAll(0, runner.test(x, y, 67, 57, 2));
+			}
+			if (xStart > 85) {
+				path.addAll(0, runner.test(x, y, 103, 57, 2));		
+			}
+		  }
+		else if (yStart > 67) {
+			if (xStart < 86)  {
+				path.addAll(0, runner.test(x, y, 63, 70, 2));
+				
+			}
+			if (xStart > 85) {
+				path.addAll(0, runner.test(x, y, 107, 70, 2));			
+			}
+		}
+		path.add(0, new Point (-1, -1));
+	}
+	public void findPortalGoingUp() {
+		int x; int y;
+		if (path.size() > 1) {
+			 x = path.get(1).x;
+			 y = path.get(1).y;
+		}
+		else {
+			x = xStart;
+			y = yStart;
+		}
+		
+		if (xDest < 43) {
+			path.addAll(0, runner.test(x, y, 26, 67, 1));
+			
+		}
+		else if (xDest <  56) {
+			if (yDest < 85)
+				path.addAll(0,runner.test(x, y, 46, 67, 1));
+			if (yDest > 84)	
+				path.addAll(0, runner.test(x, y, 52, 96, 1));
+		}
+		else if (yDest > 14 && yDest < 20) {
+			if (xDest < 86)  {
+				path.addAll(0,runner.test(x, y, 63, 16, 1));
+			}
+			if (xDest > 85) {
+				path.addAll(0,runner.test(x, y, 107, 16, 1));		
+			}
+		  }
+		else if (yDest > 19 && yDest < 37) {
+			if (xDest < 86)  {
+				path.addAll(0,runner.test(x, y, 67, 29, 1));
+				
+			}
+			if (xDest > 85) {
+				path.addAll(0,runner.test(x, y, 103, 29, 1));			
+			}
+		}
+		else if(yDest > 60 && yDest < 68) { 
+			if (xDest < 86)  {
+				path.addAll(0,runner.test(x, y, 67, 57, 1));
+			}
+			if (xDest > 85) {
+				path.addAll(0,runner.test(x, y, 103, 57, 1));		
+			}
+		  }
+		else if (yDest > 67) {
+			if (xDest < 86)  {
+				path.addAll(0,runner.test(x, y, 63, 70, 1));
+				
+			}
+			if (xDest > 85) {
+				path.addAll(0, runner.test(x, y, 107, 70, 1));			
+			}
+		}	
+		path.add(0, new Point (-1, -1));
+	}
+	
+
 	public void updatePosition() {
 
 			if (path.size()> 0) {
-				xStart = path.get(path.size()-1).x;
-				yStart = path.get(path.size()-1).y;
+				int x = path.get(path.size()-1).x;
+				int y = path.get(path.size()-1).y;
+				//System.out.println(x + "  " + y + "   " + path.size());
+				if (x==-1 & y==-1) {
+					if (floorStart == 2)floorStart--;
+					else {floorStart++;}
+					//path.addAll()
+				}
+				else {
+					xStart = x;
+					yStart = y;
+				}
 				path.remove(path.size()-1);
 			}
 
 		
 		
-	}
+	}  
 	public void setColor(Color col) {
 		color = col;
 	}
